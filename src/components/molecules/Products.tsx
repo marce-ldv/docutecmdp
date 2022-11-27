32531860e98f7e41073a59efa612142b846f618c
marceloludovino <marcelo.docutec@gmail.com>
2022-11-27 16:28:53 -0300

    fix: typo

--- src/components/molecules/Products.tsx
+++ src/components/molecules/Products.tsx
@@ -1,14 +1,15 @@
 import { Card } from "../atoms/Card";
+import { uid } from "uid";
 
 export const Products = () => {
   return (
     <>
       <div className="w-full bg-gray-800">
-        {/* Remove py-8 */}
         <div className="container mx-auto py-8">
           <div className="flex flex-wrap items-center justify-center gap-y-4 gap-x-1 lg:justify-between">
             {[...Array(10)].map((product) => (
               <Card
+                key={uid()}
                 title={"iphone XS"}
                 description={
                   "The Apple iPhone XS is available in 3 colors with 64GB memory. Shoot amazing videos"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { Navbar } from "../components/molecules/Navbar";
 import { Footer } from "../components/molecules/Footer";
-import { Card } from "../components/atoms/Card";
-import { Carousel } from "../components/atoms/Carousel";
 import { Brands } from "../components/molecules/Brands";
 import { Hero } from "../components/molecules/Hero";
 import { Products } from "../components/molecules/Products";
@@ -20,14 +17,6 @@ const Home: NextPage = () => {
       <div>
         <Navbar />
         <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#000d51] to-[#15162c]">
-          {/*<Card*/}
-          {/*  title={"lorem2"}*/}
-          {/*  description={"asadsadsadsads"}*/}
-          {/*  btnLabel={"buy"}*/}
-          {/*  image={"https://flowbite.com/docs/images/blog/image-1.jpg"}*/}
-          {/*/>*/}
-
-          {/*<Carousel items={[{},{},{},{},{},{},{},{}, {}]} />*/}
           <Hero />
           <Brands />
           <Products />
